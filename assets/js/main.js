6efe1ffb7837a5a2674007e60d41effdc4e8f0ac
grehov084 <grehov084@gmail.com>
2024-04-05 14:52:23 +0700

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
-let burgerBtn, mobileContent;
+let burgerBtn, mobileContent, advantegesContent, advantegesItem;
 
 burgerBtn = document.querySelector(".site-header-burger");
 mobileContent = document.querySelector(".site-header-mobile");
+advantegesContent = document.querySelector(".site-content-advantages");
+
+
 burgerBtn.addEventListener("click", ()=>{
     if(!mobileContent.hasAttribute("style")){
         mobileContent.style.cssText = "transform: translate(0, 0);";
     }
     else{
         mobileContent.removeAttribute("style");
+    }
+});
+document.addEventListener("scroll", ()=>{
+    console.log(advantegesContent.getBoundingClientRect().top);
+    if(advantegesContent.getBoundingClientRect().top <= 7.9375){
+        console.log("touch");
     }
 });
